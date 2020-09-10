2fa87fb773737d8d71e310cb02f1da38757d96eb
Patrick Jones <patrickjones.pmj@gmail.com>
2020-09-10 01:26:31 -0500

    add tailwind and configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
+const resolveConfig = require("tailwindcss/resolveConfig")
+const tailwindConfig = require("./tailwind")
+const fullConfig = resolveConfig(tailwindConfig)
+
 module.exports = {
   siteMetadata: {
     title: `Patrick Jones - Software Engineer`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@forksofpower`
+    author: `@forksofpower`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -17,14 +21,40 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `./src/data`
-      }
+        path: `./src/data`,
+      },
     },
     {
-      resolve: 'gatsby-source-dev',
+      resolve: "gatsby-source-dev",
       options: {
         // your Dev.to username
-        username: 'forksofpower'
+        username: "forksofpower",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-prefetch-google-fonts",
+      options: {
+        fonts: [
+          { 
+            family: `Space Mono`, 
+            variants: [`400`, `700`]
+          }, { 
+            family: `Roboto`,
+            variants: [`400`, `700`]
+          }
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [
+          require(`tailwindcss`)(tailwindConfig),
+          require(`autoprefixer`),
+          ...(process.env.NODE_ENV === `production`
+            ? [require(`cssnano`)]
+            : []),
+        ],
       },
     },
     `gatsby-plugin-emotion`,
@@ -41,7 +71,7 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
